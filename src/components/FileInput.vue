ada080cf5cdbd1ddbd51bf1a6320397d13d11195
Olga Kashapova <invispo16@gmail.com>
2024-04-18 14:03:17 +0300

    design part almost done

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -1,23 +1,93 @@
 <template>
-  <div class="content">
-    <h4 style="display:block">
-      Label
+  <div class="file-input">
+    <h4 class="file-input__label">
+      {{ labelText }}
     </h4>
-    <div>
-
+    <div class="file-input__form">
+      <div>
+        <FileInputButton
+            :button-text="buttonText"
+            @file-selected="onFileSelect($event)"
+            @cancel="onCancel"
+        />
+      </div>
+      <div class="file-input__file-info">
+        <LoadingSpinner :loading="loading"/>
+        <span :class="fileName ? 'file_input__file-name_loaded' : 'file_input__file-name_init'">
+          {{ fileName ?? 'Файл не выбран' }}
+        </span>
+      </div>
     </div>
-    <FileInputButton button-text="Выбрать файл"/>
-
-    <h4 style="display:block">
-      Hint
+    <h4 class="file-input__hint">
+      {{ hintText }}
     </h4>
   </div>
 </template>
 
 <script setup lang="ts">
-
 import FileInputButton from "./FileInputButton.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
+import {computed, ref} from "vue";
+
+const fileName = ref<string | null>(null);
+
+const loading = ref<boolean>(false);
+
+const labelText = ref('Label');
+const hintText = ref('Hint text');
+
+const buttonText = computed(() => {
+  if (loading.value) {
+    return 'Отменить';
+  }
+  else if (!loading.value && fileName.value) {
+    return 'Удалить';
+  }
+  else {
+    return 'Выбрать файл';
+  }
+});
+
+const onFileSelect = (e: Event) => {
+  loading.value = true;
+  setTimeout(() => {
+    loading.value = false;
+    fileName.value = (e.target as HTMLInputElement)!.files![0].name;
+  }, 1000)
+};
+
+const onCancel = () => {
+
+};
 </script>
 
 <style scoped>
+.file-input {
+  margin: 1em;
+}
+
+.file-input__form {
+  display: flex;
+  column-gap: 1.2em;
+  margin: 1.5em 0;
+}
+
+.file-input__file-info {
+  display: flex;
+  align-content: center;
+  column-gap: 0.5em;
+}
+
+.file-input__hint {
+  font-weight: normal;
+  color: #6B7280;
+}
+
+.file_input__file-name_loaded {
+  color: #6B7280;
+}
+
+.file_input__file-name_init {
+  color: #9BA2AF;
+}
 </style>
--- src/components/FileInputButton.vue
+++ src/components/FileInputButton.vue
@@ -1,40 +1,48 @@
 <template>
-  <input type="file"/>
+  <label for="file-upload" class="input-button">
+    {{ buttonText }}
+  </label>
+  <input
+      type="file"
+      id="file-upload"
+      @change="emit('fileSelected', $event)"
+      @cancel="emit('cancel')"
+  />
 </template>
 
 <script setup lang="ts">
+
 interface Props {
   buttonText: string;
 }
 
 defineProps<Props>();
-const emit = defineEmits(['clicked']);
+
+const emit = defineEmits(['fileSelected', 'cancel']);
 </script>
 
 <style scoped>
+input[type="file"] {
+  display: none;
+}
 
-input::file-selector-button {
+.input-button {
+  padding: 0.8em 0.9em;
   background-color: white;
   color: #384252;
-  padding: 0.8em 0.9em;
   border-radius: 0.8em;
   border: 1px solid #e5e7eb;
   cursor: pointer;
   box-shadow: 0 2px 7px -6px rgba(0,0,0,0.75);
-  font-family: sans-serif;
   transition: background-color 0.4s;
 }
 
-.button:hover {
+.input-button:hover {
   background-color: #F3F4F6;
 }
 
-.button:active {
+.input-button:active {
   background-color: #D0D5DC;
   box-shadow: none;
-}
-
-.button:focus {
-  outline: 2px solid #F0F1F2 !important;
 }
 </style>
